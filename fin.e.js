e7f9ed1b9786c6bcf937be9501da546547362432
iki1uc <entertrainer@web.de>
2026-07-11 03:05:32 +0200

    Create fin.e.js

---
+++ fin.e.js
@@ -0,0 +1,6 @@
+// RAUM-ID Kernel
+function GHOST_SCAN_REAL(r){ ... }   // wie oben
+function RAUM_KERNEL(raum){ ... }    // wie oben
+function MOVE(raum){ ... }           // wie oben
+
+console.log("RAUM-ID Kernel geladen.");
